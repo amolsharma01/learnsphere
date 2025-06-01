6280f0a35f2335cadd6ce6086358b05af318ef4d
Amol Sharma <124274806+amolsharma01@users.noreply.github.com>
2025-06-01 18:18:53 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "https://learnsphere-3-bi9g.onrender.com";
+export const server = "https://learnsphere-4-5me4.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
